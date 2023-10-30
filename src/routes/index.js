704bdfd67fd6bf4cd12c396322a8309cb92bf5a6
Juan HuVars <104521110+Juan1990Vars@users.noreply.github.com>
2023-10-29 20:24:52 -0600

    users backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const userRouter = require('./user.router');
 const router = express.Router();
 
-// colocar las rutas aquí
-
+// colocar las rutas aquí, users es el nombre que asigne a mi ruta
+router.use('/users', userRouter);
 
 module.exports = router;
